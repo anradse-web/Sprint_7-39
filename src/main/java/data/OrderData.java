a9fe00dea9adc4de1d9142ebc4c356a341dfb46b
anradse-web <an.rad.se@gmail.com>
2026-03-13 20:39:45 +0300

    Add files via upload

--- src/main/java/data/OrderData.java
+++ src/main/java/data/OrderData.java
@@ -1,7 +1,6 @@
 package data;
 
 public class OrderData {
-    public static final String BASE_URI = "https://qa-scooter.praktikum-services.ru/";
     public static String ORDER_POST_CREATE = "/api/v1/orders"; // Создание заказа
     public static String ORDER_GET_LIST = "/api/v1/orders"; // Получение списка заказов
     public static String ORDERS_CANCEL = "/api/v1/orders/cancel";// Омена заказов
